49d82dd728800b93daee90d3eedc82957dfb084b
cppAli <romankoalice@gmail.com>
2023-07-08 23:43:30 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+List<BasicOperation> operations = new List<BasicOperation>();
+string json = "history.json";
+
+bool exit = false;
+    while (!exit)
+    {
+        Console.WriteLine("Select calculation operation:");
+        Console.WriteLine("1. Addition");
+        Console.WriteLine("2. Subtraction");
+        Console.WriteLine("3. Multiplication");
+        Console.WriteLine("4. Division");
+        Console.WriteLine("5. Show transaction history");
+        Console.WriteLine("6. Clear transaction history");
+        Console.WriteLine("7. Exit");
+
+    string choice = Console.ReadLine();
+
+        switch (choice)
+        {
+            case "1":
+                AddOperation("+");
+                break;
+            case "2":
+                AddOperation("-");
+                break;
+            case "3":
+                AddOperation("*");
+                break;
+            case "4":
+                AddOperation("/");
+                break;
+            case "5":
+                ShowHistory();
+                break;
+            case "6":
+                ClearHistory();
+                break;
+            case "7":
+                exit = true;
+                break;
+            default:
+                Console.WriteLine("Invalid operation selection.");
+                break;
+        }
+    }
+void AddOperation(string operationType)
+{
+    Console.Write("Enter the first number: ");
+    double num1 = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Enter the second number: ");
+    double num2 = Convert.ToDouble(Console.ReadLine());
+
+    double result = 0;
+    switch (operationType)
+    {
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        case "*":
+            result = num1 * num2;
+            break;
+        case "/":
+            if (num2 != 0)
+                result = num1 / num2;
+            else
+                Console.WriteLine("Division by zero is not possible.");
+            break;
+        default:
+            Console.WriteLine("Invalid operation type.");
+            break;
+    }
+
+    BasicOperation mathOperation = new BasicOperation(num1, num2, operationType, result);
+    operations.Add(mathOperation);
+
+    Console.WriteLine($"Result: {result}");
+    Console.WriteLine();
+
+    SaveHistoryToJson();
+}
+
+void ShowHistory()
+{
+    List<BasicOperation> history = BasicOperation.LoadHistoryFromJson(json);
+    BasicOperation.ShowHistory(history);
+}
+
+void SaveHistoryToJson()
+{
+    BasicOperation.SaveHistoryToJson(operations, json);
+    Console.WriteLine("Transaction history has been saved to the JSON file.");
+}
+void ClearHistory()
+{
+    BasicOperation.ClearHistory(json);
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿//отдельный.cs
+using System;
+using System.Text.Json;
+
+public class BasicOperation
+{
+    public double Num1 { get; set; }
+    public double Num2 { get; set; }
+    public string OperationType { get; set; }
+    public double Result { get; set; }
+
+    public BasicOperation(double num1, double num2, string operationType, double result)
+    {
+        Num1 = num1;
+        Num2 = num2;
+        OperationType = operationType;
+        Result = result;
+    }
+
+    public static void ShowHistory(List<BasicOperation> operations)
+    {
+        if (operations.Count == 0)
+        {
+            Console.WriteLine("Operation history is empty.");
+            Console.WriteLine();
+            return;
+        }
+
+        Console.WriteLine("Operations history:");
+        foreach (BasicOperation operation in operations)
+        {
+            Console.WriteLine($"{operation.Num1} {operation.OperationType} {operation.Num2} = {operation.Result}");
+        }
+        Console.WriteLine();
+    }
+
+    public static void SaveHistoryToJson(List<BasicOperation> operations, string filePath)
+    {
+        string json = JsonSerializer.Serialize(operations);
+        File.WriteAllText(filePath, json);
+    }
+
+    public static List<BasicOperation> LoadHistoryFromJson(string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            return new List<BasicOperation>();
+        }
+
+        string json = File.ReadAllText(filePath);
+        return JsonSerializer.Deserialize<List<BasicOperation>>(json);
+    }
+
+    public static void ClearHistory(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            File.Delete(filePath);
+        }
+
+        Console.WriteLine("Operation history has been cleared.");
+        Console.WriteLine();
+    }
+}
+
+
+
